8a5d34cd13b0dd6c8233f0158526b01b7144325f
Sebastián Ramírez <tiangolo@gmail.com>
2021-05-27 11:34:41 +0200

    ✅ Add tests, lots of tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,19 @@
-from pathlib import Path
-import subprocess
 import shutil
+import subprocess
+from pathlib import Path
+
 import pkginfo
 
 testing_assets = Path(__file__).parent / "assets"
 plugin_source_dir = Path(__file__).parent.parent / "poetry_version_plugin"
 
 
-def test_defaults(tmp_path: Path):
-    no_packages_path = testing_assets / "no_packages"
-    testing_dir = tmp_path / "testing_package"
-    shutil.copytree(no_packages_path, testing_dir)
+def copy_assets(source_name: str, testing_dir: Path):
+    package_path = testing_assets / source_name
+    shutil.copytree(package_path, testing_dir)
+
+
+def build_package(testing_dir: Path):
     result = subprocess.run(
         [
             "coverage",
@@ -27,6 +30,17 @@ def test_defaults(tmp_path: Path):
         stderr=subprocess.PIPE,
         encoding="utf-8",
     )
+    coverage_path = list(testing_dir.glob(".coverage*"))[0]
+    dst_coverage_path = Path(__file__).parent.parent / coverage_path.name
+    dst_coverage_path.write_bytes(coverage_path.read_bytes())
+    return result
+
+
+def test_defaults(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("no_packages", testing_dir)
+    result = build_package(testing_dir=testing_dir)
+
     assert (
         "Using __init__.py file at test_custom_version/__init__.py for dynamic version"
         in result.stdout
@@ -39,31 +53,12 @@ def test_defaults(tmp_path: Path):
     wheel_path = testing_dir / "dist" / "test_custom_version-0.0.1-py3-none-any.whl"
     info = pkginfo.get_metadata(str(wheel_path))
     assert info.version == "0.0.1"
-    coverage_path = list(testing_dir.glob(".coverage*"))[0]
-    dst_coverage_path = Path(__file__).parent.parent / coverage_path.name
-    dst_coverage_path.write_bytes(coverage_path.read_bytes())
 
 
 def test_custom_packages(tmp_path: Path):
-    package_path = testing_assets / "custom_packages"
     testing_dir = tmp_path / "testing_package"
-    shutil.copytree(package_path, testing_dir)
-    result = subprocess.run(
-        [
-            "coverage",
-            "run",
-            "--source",
-            str(plugin_source_dir),
-            "--parallel-mode",
-            "-m",
-            "poetry",
-            "build",
-        ],
-        cwd=testing_dir,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-    )
+    copy_assets("custom_packages", testing_dir)
+    result = build_package(testing_dir=testing_dir)
     assert (
         "Using __init__.py file at custom_package/__init__.py for dynamic version"
         in result.stdout
@@ -76,31 +71,12 @@ def test_custom_packages(tmp_path: Path):
     wheel_path = testing_dir / "dist" / "test_custom_version-0.0.2-py3-none-any.whl"
     info = pkginfo.get_metadata(str(wheel_path))
     assert info.version == "0.0.2"
-    coverage_path = list(testing_dir.glob(".coverage*"))[0]
-    dst_coverage_path = Path(__file__).parent.parent / coverage_path.name
-    dst_coverage_path.write_bytes(coverage_path.read_bytes())
 
 
 def test_variations(tmp_path: Path):
-    package_path = testing_assets / "variations"
     testing_dir = tmp_path / "testing_package"
-    shutil.copytree(package_path, testing_dir)
-    result = subprocess.run(
-        [
-            "coverage",
-            "run",
-            "--source",
-            str(plugin_source_dir),
-            "--parallel-mode",
-            "-m",
-            "poetry",
-            "build",
-        ],
-        cwd=testing_dir,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-    )
+    copy_assets("variations", testing_dir)
+    result = build_package(testing_dir=testing_dir)
     assert (
         "Using __init__.py file at test_custom_version/__init__.py for dynamic version"
         in result.stdout
@@ -113,6 +89,102 @@ def test_variations(tmp_path: Path):
     wheel_path = testing_dir / "dist" / "test_custom_version-0.0.3-py3-none-any.whl"
     info = pkginfo.get_metadata(str(wheel_path))
     assert info.version == "0.0.3"
-    coverage_path = list(testing_dir.glob(".coverage*"))[0]
-    dst_coverage_path = Path(__file__).parent.parent / coverage_path.name
-    dst_coverage_path.write_bytes(coverage_path.read_bytes())
+
+
+def test_no_version_var(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("no_version_var", testing_dir)
+    result = build_package(testing_dir=testing_dir)
+    assert (
+        "No valid __version__ variable found in __init__.py, cannot extract dynamic version"
+        in result.stderr
+    )
+    assert result.returncode != 0
+
+
+def test_no_standard_dir(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("no_standard_dir", testing_dir)
+    result = build_package(testing_dir=testing_dir)
+    assert "__init__.py file not found at" in result.stderr
+    assert result.returncode != 0
+
+
+def test_multiple_packages(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("multiple_packages", testing_dir)
+    result = build_package(testing_dir=testing_dir)
+    assert "More than one package set, cannot extract dynamic version" in result.stderr
+    assert result.returncode != 0
+
+
+def test_no_config(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("no_config", testing_dir)
+    result = build_package(testing_dir=testing_dir)
+    assert "Built test_custom_version-0-py3-none-any.whl" in result.stdout
+    assert result.returncode == 0
+
+
+def test_no_config_source(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("no_config_source", testing_dir)
+    result = build_package(testing_dir=testing_dir)
+    assert (
+        "No source configuration found in [tool.poetry-version-plugin] "
+        "in pyproject.toml, not extracting dynamic version"
+    ) in result.stderr
+    assert result.returncode != 0
+
+
+def test_git_tag(tmp_path: Path):
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("git_tag", testing_dir)
+    result = result = subprocess.run(
+        [
+            "git",
+            "init",
+        ],
+        cwd=testing_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert result.returncode == 0
+    result = result = subprocess.run(
+        ["git", "add", "."],
+        cwd=testing_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert result.returncode == 0
+    result = result = subprocess.run(
+        ["git", "commit", "-m", "release"],
+        cwd=testing_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert result.returncode == 0
+    result = build_package(testing_dir=testing_dir)
+    assert "No Git tag found, not extracting dynamic version" in result.stderr
+    assert result.returncode != 0
+    result = result = subprocess.run(
+        [
+            "git",
+            "tag",
+            "0.0.9",
+        ],
+        cwd=testing_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert result.returncode == 0
+    result = build_package(testing_dir=testing_dir)
+    assert "Git tag found, setting dynamic version to: 0.0.9" in result.stdout
+    assert "Built test_custom_version-0.0.9-py3-none-any.whl" in result.stdout
+    wheel_path = testing_dir / "dist" / "test_custom_version-0.0.9-py3-none-any.whl"
+    info = pkginfo.get_metadata(str(wheel_path))
+    assert info.version == "0.0.9"
